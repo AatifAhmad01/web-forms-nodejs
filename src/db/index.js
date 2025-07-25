4773b8eda3865b564abb4e77003cd6d13dbbf116
Atif Ahmad <40195767+AatifAhmad01@users.noreply.github.com>
2025-07-25 12:30:37 +0430

    init project

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const res = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Connected to MongoDB: ${res.connection.host}`);
+    } catch (error) {
+        console.log(`Error connecting to MongoDB: ${error}`);
+    }
+};
+
+export default connectDB;
